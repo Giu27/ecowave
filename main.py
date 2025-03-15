0eeae760cd867ecb153674cfd5727d4972dd7eaf
Giu27 <Caruso.giuseppe09@hotmail.com>
2025-03-15 23:22:15 +0100

    A simple linear regression model

--- main.py
+++ main.py
@@ -2,10 +2,99 @@ import torch
 from torch import nn 
 import matplotlib.pyplot as plt
 import numpy as np
+from pathlib import Path
+from pprint import pprint
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
+print(f"Using device: {device}")
 
-tensor = torch.tensor([1, 2, 3])
-tensor = tensor.to(device)
+class LinearRegressionModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.linear_layer = nn.Linear(in_features=1,out_features=1)
 
-print(tensor, tensor.device)
+    def forward(self, x:torch.Tensor) -> torch.Tensor:
+        return self.linear_layer(x)
+
+#known info
+weight = 0.7
+bias = 0.3
+
+# Create data
+start = 0
+end = 1
+step = 0.02
+X = torch.arange(start, end, step).unsqueeze(dim=1)
+y = weight * X + bias
+
+# Create train/test split
+train_split = int(0.8 * len(X)) # 80% of data used for training set, 20% for testing 
+X_train, y_train = X[:train_split], y[:train_split]
+X_test, y_test = X[train_split:], y[train_split:]
+
+def plot_predictions(train_data=X_train, train_labels=y_train, test_data=X_test, test_labels=y_test, predictions=None):
+    """Plots training data, test data and compares predictions."""
+
+    plt.figure(figsize=(10, 7))
+    plt.scatter(train_data, train_labels, c="b", s=4, label="Training data")
+    plt.scatter(test_data, test_labels, c="g", s=4, label="Testing data")
+
+    if predictions is not None:
+        plt.scatter(test_data, predictions, c="r", s=4, label="Predictions")
+    plt.legend(prop={"size": 14})
+
+torch.manual_seed(42)
+
+model_0 = LinearRegressionModel()
+model_0.to(device)
+
+# Create the loss function
+loss_fn = nn.L1Loss()
+
+# Create the optimizer
+optimizer = torch.optim.SGD(params=model_0.parameters(), lr=0.01)
+
+epochs = 1000
+
+X_train = X_train.to(device)
+X_test = X_test.to(device)
+y_train = y_train.to(device)
+y_test = y_test.to(device)
+
+for epoch in range(epochs):
+    #training
+    model_0.train()
+    y_pred = model_0(X_train)
+    loss = loss_fn(y_pred, y_train)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    #testing
+    model_0.eval()
+    with torch.inference_mode():
+        test_pred = model_0(X_test)
+        test_loss = loss_fn(test_pred, y_test)
+
+        if epoch % 100 == 0:
+            print(f"Epoch: {epoch} | Train Loss: {loss} | Test Loss: {test_loss} ")
+
+print("The model learned the following values for weights and bias:")
+pprint(model_0.state_dict())
+print("\nAnd the original values for weights and bias are:")
+print(f"weights: {weight}, bias: {bias}")
+
+model_0.eval()
+with torch.inference_mode():
+    y_preds = model_0(X_test)
+
+plot_predictions(predictions=y_preds.cpu())
+
+MODEL_PATH = Path("models")
+MODEL_PATH.mkdir(parents=True, exist_ok=True)
+
+MODEL_NAME = "01_pytorch_workflow_model_0.pth"
+MODEL_SAVE_PATH = MODEL_PATH / MODEL_NAME
+
+print(f"Saving model to: {MODEL_SAVE_PATH}")
+torch.save(obj=model_0.state_dict(),f=MODEL_SAVE_PATH)
