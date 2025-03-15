80f2c90a6614e51ed08978e8908991a3592d0479
Giu27 <Caruso.giuseppe09@hotmail.com>
2025-03-15 20:54:49 +0100

    code now works on the gpu if available

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import torch
 from torch import nn 
 import matplotlib.pyplot as plt
+import numpy as np
 
-tensor_A = torch.tensor([[1, 2],
-                         [3, 4],
-                         [5, 6]], dtype=torch.float32)
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
-tensor_B = torch.tensor([[7, 10],
-                         [8, 11], 
-                         [9, 12]], dtype=torch.float32)
-print(torch.matmul(tensor_A, tensor_B.T))
+tensor = torch.tensor([1, 2, 3])
+tensor = tensor.to(device)
+
+print(tensor, tensor.device)
